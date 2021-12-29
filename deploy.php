331adebe923d074f60cc1a6b76e4c12f9fee6fce
Elias Häußler <elias@haeussler.dev>
2021-12-29 17:03:44 +0100

    [BUGFIX] Correctly set language parameters for deployment

--- deploy.php
+++ deploy.php
@@ -29,7 +29,6 @@ require 'recipe/symfony.php';
 set('repository', 'https://github.com/eliashaeussler/typo3-badges.git');
 set('env', [
     'APP_ENV' => 'prod',
-    'LANG' => 'en_US.UTF-8',
 ]);
 
 // Hosts
