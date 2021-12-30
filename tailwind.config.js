763ef19e8b0f5a412085bf619c8a14d1381df210
Elias Häußler <elias@haeussler.dev>
2021-12-30 15:32:49 +0100

    [FEATURE] Add homepage

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './templates/**/*.html.twig'
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Lato', 'sans-serif'],
+      'mono': ['JetBrains Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        'typo3-orange': '#f49700',
+      },
+    },
+  },
+  plugins: [],
+}
