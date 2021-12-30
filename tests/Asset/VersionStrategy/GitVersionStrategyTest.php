074b4fc372f0658bb8c3ff00b863629946076068
Elias Häußler <elias@haeussler.dev>
2021-12-30 23:52:56 +0100

    [FEATURE] Implement cache busting for assets

---
+++ tests/Asset/VersionStrategy/GitVersionStrategyTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Symfony project "eliashaeussler/typo3-badges".
+ *
+ * Copyright (C) 2021 Elias Häußler <elias@haeussler.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Tests\Asset\VersionStrategy;
+
+use App\Asset\VersionStrategy\GitVersionStrategy;
+use PHPUnit\Framework\TestCase;
+use SebastianFeldmann\Git\Repository;
+
+/**
+ * GitVersionStrategyTest.
+ *
+ * @author Elias Häußler <elias@haeussler.dev>
+ * @license GPL-3.0-or-later
+ */
+final class GitVersionStrategyTest extends TestCase
+{
+    private GitVersionStrategy $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = new GitVersionStrategy();
+    }
+
+    /**
+     * @test
+     */
+    public function getVersionReturnsCurrentCommitHash(): void
+    {
+        $expected = substr((new Repository())->getInfoOperator()->getCurrentCommitHash(), 0, 7);
+
+        self::assertSame($expected, $this->subject->getVersion('/foo'));
+    }
+
+    /**
+     * @test
+     */
+    public function applyVersionsReturnsAssetWithCurrentCommitHash(): void
+    {
+        $version = substr((new Repository())->getInfoOperator()->getCurrentCommitHash(), 0, 7);
+        $expected = '/foo?v='.$version;
+
+        self::assertSame($expected, $this->subject->applyVersion('/foo'));
+    }
+}
