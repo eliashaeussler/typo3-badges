a1ee0d8a10ebd42801f847635678f0d5f7f3b6f6
Elias Häußler <elias@haeussler.dev>
2022-12-23 20:06:05 +0100

    [TASK] Update Rector set lists

--- rector.php
+++ rector.php
@@ -38,9 +38,10 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
         SymfonySetList::SYMFONY_60,
+        SymfonySetList::SYMFONY_61,
+        SymfonySetList::SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::SYMFONY_STRICT,
     ]);
 
     $rectorConfig->skip([
