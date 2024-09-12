03da1ce790c1a0defa87cf0a48413096ea14fc1d
Elias Häußler <elias@haeussler.dev>
2024-09-12 14:06:59 +0200

    [TASK] Remove dependency to "eslint-plugin-sonarjs"

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 const globals = require('globals');
 const js = require('@eslint/js');
-const sonarjs = require('eslint-plugin-sonarjs');
 
 module.exports = [
   js.configs.recommended,
-  sonarjs.configs.recommended,
   {
     languageOptions: {
       globals: {
