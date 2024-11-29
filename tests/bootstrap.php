366b93bdc895ca854fe87f1c1dbd6d991da16669
Elias Häußler <elias@haeussler.dev>
2024-11-29 16:57:16 +0100

    [TASK] Remove superfluous check

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -26,9 +26,7 @@ use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ((bool) $_SERVER['APP_DEBUG']) {
     umask(0000);
