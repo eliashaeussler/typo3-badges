f339c3cfdbfcc49e03e577ee261c966355642083
Elias Häußler <elias@haeussler.dev>
2026-05-13 16:54:16 +0200

    [TASK] Increase version ranges of `typo3_version` filter parameter

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -84,7 +84,7 @@ final readonly class ApiService
                     'page' => random_int(1, 10),
                     'per_page' => 20,
                     'filter' => [
-                        'typo3_version' => random_int(9, 13),
+                        'typo3_version' => random_int(11, 14),
                     ],
                 ];
                 $apiUrl = $apiPath.'?'.http_build_query($filterOptions);
